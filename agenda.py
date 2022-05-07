5f1446997a601409f26291ca8f7074693b069834
Igor Lima <igorlimaesilva94@gmail.com>
2022-05-06 22:54:37 -0300

    organização de codigo

--- agenda.py
+++ agenda.py
@@ -229,11 +229,18 @@ def data_hora():
     dia = date.today()
     dia_string = str(dia).split("-")
     dia_format = dia_string[2] + '/' + dia_string[1] + '/' + dia_string[0]
-    dia_format_ext = dia_format.replace("/01/", "/jan/").replace("/02/", "/fev/").replace("/03/",
-                                                                                          "/mar/").replace(
-        "/04/", "/abr/").replace("/05/", "/mai/").replace("/06/", "/jun/").replace("/07/", "/jul/").replace("/08/",
-                                                                                                            "/ago/").replace(
-        "/09/", "/set/").replace("/10/", "/out/").replace("/11/", "/nov/").replace("/12/", "/dez/")
+    dia_format_ext = dia_format.replace("/01/", "/jan/") \
+        .replace("/02/", "/fev/") \
+        .replace("/03/", "/mar/") \
+        .replace("/04/", "/abr/") \
+        .replace("/05/", "/mai/") \
+        .replace("/06/", "/jun/") \
+        .replace("/07/", "/jul/") \
+        .replace("/08/", "/ago/") \
+        .replace("/09/", "/set/") \
+        .replace("/10/", "/out/") \
+        .replace("/11/", "/nov/") \
+        .replace("/12/", "/dez/")
 
     hora = datetime.now()
     hora_atual = hora.strftime("%H:%M")
